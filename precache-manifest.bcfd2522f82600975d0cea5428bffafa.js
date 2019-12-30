cd110ba295c8e990d89d7be1adaaab02f6edb1c9
Pierina Zaramella <ext_pzaramella@falabella.cl>
2019-12-30 19:01:57 -0300

    Updates

---
+++ precache-manifest.bcfd2522f82600975d0cea5428bffafa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8e7e2071d130fba30760ffe29aa86e",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "4404565709cff8b7a8f5",
+    "url": "/music-player/static/css/main.09f0fac0.chunk.css"
+  },
+  {
+    "revision": "e8fa9ee8ca222f2cdebd",
+    "url": "/music-player/static/js/2.3de61bf2.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/music-player/static/js/2.3de61bf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4404565709cff8b7a8f5",
+    "url": "/music-player/static/js/main.f1713116.chunk.js"
+  },
+  {
+    "revision": "5a1d71b9760596fc4e6c",
+    "url": "/music-player/static/js/runtime-main.d07ca20b.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/music-player/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
