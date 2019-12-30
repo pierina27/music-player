53c2f25b3965ed99e425d106780d766b5307f475
Pierina Zaramella <ext_pzaramella@falabella.cl>
2019-12-30 18:41:50 -0300

    source to publish

---
+++ build/precache-manifest.66384c81d510a043a4bbc0dc32abc7b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16e3ec4171010bf9b6dac19b9e490bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "711a35eed9911c9638ba",
+    "url": "/static/css/main.654aa16a.chunk.css"
+  },
+  {
+    "revision": "e8fa9ee8ca222f2cdebd",
+    "url": "/static/js/2.3de61bf2.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/static/js/2.3de61bf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "711a35eed9911c9638ba",
+    "url": "/static/js/main.fccaad3e.chunk.js"
+  },
+  {
+    "revision": "b15384f7335ccf709a4a",
+    "url": "/static/js/runtime-main.c6f17d9c.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
